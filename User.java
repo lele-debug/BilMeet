bf03de71c2a7fad3853b2579d72b27129d1900ea
Nehir Bakdur <nehir.bakdur@gmail.com>
2026-02-18 21:38:54 +0300

    Added friends list for users and methods for managing friends and interests

--- User.java
+++ User.java
@@ -6,14 +6,23 @@ public class User {
     private String userPassword;
     private String userMail;
     private ArrayList<String> userInterests;
+    private ArrayList<User> userFriends;
 
     public User(String userName, String userPassword, String userMail) {
         this.userName = userName;
         this.userPassword = userPassword;
         this.userMail = userMail;
-        userInterests = new ArrayList<String>();  //create an empty arraylist of interests for now
+        this.userInterests = new ArrayList<String>();  //create an empty arraylist of interests for now
+        this.userFriends = new ArrayList<User>();      //create an empty arraylist of friends for now
     }
 
+    //Adder methods
+    public void addFriend(User newFriend) { userFriends.add(newFriend); }
+    public void addInterest(String interest) { userInterests.add(interest); }
+
+    //Remover methods
+    public void removeFriend(User friend) { userFriends.remove(friend); }
+    public void removeInterest(String interest) { userInterests.remove(interest); }
 
     //Setter methods
     public void setUserName(String newName) { this.userName = newName; }
